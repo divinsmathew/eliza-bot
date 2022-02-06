2dea87ab9ff0cd20d452a10883d6b2d41a743ec6
Divins Mathew <divinsmathew@gmail.com>
2022-02-07 00:48:16 +0530

    Add project files.

---
+++ Eliza/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Eliza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BigBox = new System.Windows.Forms.RichTextBox();
+            this.QuerryBox = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BigBox
+            // 
+            this.BigBox.Location = new System.Drawing.Point(11, 37);
+            this.BigBox.Name = "BigBox";
+            this.BigBox.ReadOnly = true;
+            this.BigBox.Size = new System.Drawing.Size(407, 196);
+            this.BigBox.TabIndex = 33;
+            this.BigBox.Text = "";
+            // 
+            // QuerryBox
+            // 
+            this.QuerryBox.Location = new System.Drawing.Point(12, 243);
+            this.QuerryBox.Name = "QuerryBox";
+            this.QuerryBox.Size = new System.Drawing.Size(325, 20);
+            this.QuerryBox.TabIndex = 0;
+            this.QuerryBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.QuerryBox_KeyUp);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Enabled = false;
+            this.SendButton.Location = new System.Drawing.Point(343, 242);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 23);
+            this.SendButton.TabIndex = 2;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(254, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Powered by masswerk.at/elizabot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(12, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "diozz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(108, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(239, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Eliza, the Psychotherapist.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 291);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.QuerryBox);
+            this.Controls.Add(this.BigBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Eliza Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox BigBox;
+        private System.Windows.Forms.TextBox QuerryBox;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Eliza/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eliza
+{
+    public partial class Form1 : Form
+    {
+        WebBrowser webBrowser;
+        SpeechSynthesizer synthesizer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser = new WebBrowser();
+            webBrowser.Navigate("https://www.masswerk.at/elizabot/");
+            webBrowser.Navigated += WebBrowser_Navigated;
+
+            synthesizer = new SpeechSynthesizer();
+            synthesizer.Volume = 100;  // 0...100
+            synthesizer.Rate = 1;     // -10...10
+            try { synthesizer.SelectVoice(synthesizer.GetInstalledVoices()[1].VoiceInfo.Name); } catch { }
+        }
+
+        private async void WebBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            await Task.Delay(1000);
+            BigBox.Text = webBrowser.Document.GetElementsByTagName("textarea")[0].GetAttribute("value");
+            synthesizer.SpeakAsync(BigBox.Text.Substring(BigBox.Text.LastIndexOf(":")));
+            SendButton.Enabled = true;
+
+            QuerryBox.TabIndex = 0;
+            QuerryBox.Focus();
+        }
+
+        private async void SendButton_Click(object sender, EventArgs e)
+        {
+            webBrowser.Document.GetElementsByTagName("input")[0].SetAttribute("value", QuerryBox.Text.Trim());
+            webBrowser.Document.GetElementsByTagName("input")[1].InvokeMember("click");
+            QuerryBox.Text = "";
+
+            await Task.Delay(100);
+
+            BigBox.Text = webBrowser.Document.GetElementsByTagName("textarea")[0].GetAttribute("value");
+            synthesizer.SpeakAsync(BigBox.Text.Substring(BigBox.Text.LastIndexOf(":")));
+
+            BigBox.SelectionStart = BigBox.Text.Length;
+            BigBox.ScrollToCaret();
+
+            QuerryBox.TabIndex = 0;
+            QuerryBox.Focus();
+        }
+
+        private void QuerryBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+                SendButton_Click(null, null);
+        }
+    }
+}
